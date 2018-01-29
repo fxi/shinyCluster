7dc88812e007a5afe1ae0307b1e697111f1c7052
fxi <f@fxi.io>
2018-01-29 18:17:21 +0100

    Better dir detection

--- utils/runApp.R
+++ utils/runApp.R
@@ -6,12 +6,24 @@ path <- file.path(args[2])
 port <- as.integer(args[1])
 Sys.setlocale("LC_ALL", 'en_US.UTF-8')
 
-setwd(path)
+dirOk <- dir.exists(path);
+fileOk <- file.exists(path);
+finalPath <- path;
 
-shiny::runApp(  
-  port = port,
-  launch.browser = FALSE,
-  host = "0.0.0.0"
-  )
+if(!dirOk && fileOk){
+  finalPath <- dirname(path)
+}
 
-return(TRUE)
+dirFinalOk <- dir.exists(finalPath)
+
+if(dirFinalOk){
+  setwd(finalPath)
+  shiny::runApp(  
+    port = port,
+    launch.browser = FALSE,
+    host = "0.0.0.0"
+    )
+}else{
+  stop(paste(path, "not found"))
+}
+
